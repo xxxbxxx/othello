a1af8d0dbca35d3c0be5f29958358aac333c77db
Xavier Bouchoux <xavierb@gmail.com>
2024-08-27 09:24:28 +0200

    mainloop

--- src/main.zig
+++ src/main.zig
@@ -2,10 +2,21 @@
 const std = @import("std");
 const rl = @import("raylib");
 const gui = @import("raygui");
+const othello = @import("othello.zig");
 
 const assert = std.debug.assert;
 
+const Vec2 = rl.Vector2;
+
 pub fn main() anyerror!void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    defer {
+        std.testing.expect(gpa.deinit() == .ok) catch unreachable;
+    }
+
+    var frame_arena = std.heap.ArenaAllocator.init(gpa.allocator());
+    defer frame_arena.deinit();
+
     // Initialization
     //--------------------------------------------------------------------------------------
     const screenWidth = 1000;
@@ -15,19 +26,33 @@ pub fn main() anyerror!void {
     rl.initWindow(screenWidth, screenHeight, "othello");
     defer rl.closeWindow(); // Close window and OpenGL context
 
-    rl.setTargetFPS(60); // Set our game to run at 60 frames-per-second
+    rl.setTargetFPS(10); // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
 
     const main_board_pos: Vec2 = .{ .x = 20, .y = 20 };
     const main_board_size: f32 = 640;
 
-    var board = init_board;
-    var nextcol: Color = .white;
+    var board = othello.init_board;
+    var nextcol: othello.Color = .black;
     var showhelpers = true;
+    var game_over = false;
 
     // Main game loop
     while (!rl.windowShouldClose()) { // Detect window close button or ESC key
-        const clicked_square: ?V2i = sq: {
+        _ = frame_arena.reset(.retain_capacity);
+        const frame_alloc = frame_arena.allocator();
+
+        if (!game_over) {
+            const can_play = othello.computeValidSquares(board, nextcol) != 0;
+            if (!can_play) {
+                nextcol = if (nextcol == .white) .black else .white;
+                if (othello.computeValidSquares(board, nextcol) == 0)
+                    game_over = true;
+            }
+        }
+
+        const clicked_square: ?othello.Coord = sq: {
+            if (game_over) break :sq null;
             if (rl.isMouseButtonPressed(rl.MouseButton.mouse_button_left)) {
                 const p = Vec2.multiply(
                     Vec2.subtract(rl.getMousePosition(), main_board_pos),
@@ -40,7 +65,7 @@ pub fn main() anyerror!void {
         };
 
         if (clicked_square) |sq| play: {
-            board = play(board, sq, nextcol) catch break :play;
+            board = othello.playAt(board, sq, nextcol) catch break :play;
             nextcol = if (nextcol == .white) .black else .white;
         }
 
@@ -55,15 +80,30 @@ pub fn main() anyerror!void {
             drawHelper(board, nextcol, main_board_pos, main_board_size);
 
         rl.drawText("Next: ", 700, 50, 30, rl.Color.light_gray);
-        drawPawn(.{ .x = 815, .y = 65 }, 20, nextcol);
+        if (!game_over)
+            drawPawn(.{ .x = 815, .y = 65 }, 20, nextcol);
+
+        const score = othello.computeScore(board);
+        const scoretxt = try std.fmt.allocPrintZ(frame_alloc, "Score: {} - {}", .{ score.whites, score.blacks });
+        rl.drawText(scoretxt, 700, 100, 30, rl.Color.light_gray);
+
         _ = gui.guiCheckBox(.{ .x = 700, .y = 500, .width = 20, .height = 20 }, "show helpers", &showhelpers);
+
+        if (game_over) {
+            rl.drawText("Game Over!", 175, 200, 125, rl.Color.light_gray);
+            if (gui.guiButton(.{ .x = 400, .y = 350, .width = 100, .height = 50 }, "Restart") != 0) {
+                game_over = false;
+                board = othello.init_board;
+                nextcol = .black;
+            }
+        }
     }
 }
 
 fn addmul(p: Vec2, k: f32, d: Vec2) Vec2 {
     return Vec2.add(p, Vec2.multiply(d, .{ .x = k, .y = k }));
 }
-fn drawBoard(b: Board, pos: Vec2, size: f32) void {
+fn drawBoard(b: othello.Board, pos: Vec2, size: f32) void {
     const rect: rl.Rectangle = .{ .x = pos.x, .y = pos.y, .width = size, .height = size };
     rl.drawRectangleRounded(rect, 0.1, 7, rl.Color.dark_green);
 
@@ -101,7 +141,7 @@ fn drawBoard(b: Board, pos: Vec2, size: f32) void {
     rl.drawRectangleRoundedLinesEx(rect, 0.1, 7, 5.0, rl.Color.black);
 }
 
-fn drawPawn(pos: Vec2, radius: f32, col: Color) void {
+fn drawPawn(pos: Vec2, radius: f32, col: othello.Color) void {
     const rgb = switch (col) {
         .empty => return,
         .white => rl.Color.beige,
@@ -112,131 +152,14 @@ fn drawPawn(pos: Vec2, radius: f32, col: Color) void {
     rl.drawCircleLinesV(pos, radius, rl.Color.black);
 }
 
-fn drawHelper(b: Board, nextcol: Color, pos: Vec2, size: f32) void {
+fn drawHelper(b: othello.Board, nextcol: othello.Color, pos: Vec2, size: f32) void {
     const pos0 = Vec2.add(pos, .{ .x = size / 16, .y = size / 16 });
 
-    const valids = computeValidSquares(b, nextcol);
+    const valids = othello.computeValidSquares(b, nextcol);
     for (0..8) |y| {
         for (0..8) |x| {
-            if (valids & Board.bitmask(x, y) == 0) continue;
+            if (valids & othello.bitmask(x, y) == 0) continue;
             rl.drawCircleLinesV(addmul(pos0, size / 8, .{ .x = @floatFromInt(x), .y = @floatFromInt(y) }), size / 20, rl.Color.red);
         }
     }
-}
-
-//  ---- board
-
-const Color = enum { empty, black, white };
-const Vec2 = rl.Vector2;
-const Board = struct {
-    occupied: u64,
-    color: u64,
-
-    fn bitmask(x: anytype, y: anytype) u64 {
-        return @as(u64, 1) << @intCast(y * 8 + x);
-    }
-    fn get(b: @This(), x: anytype, y: anytype) Color {
-        assert(x < 8 and y < 8);
-        const bit: u64 = bitmask(x, y);
-        if ((b.occupied & bit) == 0) return .empty;
-        return if ((b.color & bit) == 0) .black else .white;
-    }
-    fn set(b: *@This(), x: anytype, y: anytype, c: Color) void {
-        assert(x < 8 and y < 8);
-        const bit: u64 = bitmask(x, y);
-        switch (c) {
-            .empty => b.occupied &= ~bit,
-            .black => {
-                b.occupied |= bit;
-                b.color &= ~bit;
-            },
-            .white => {
-                b.occupied |= bit;
-                b.color |= bit;
-            },
-        }
-    }
-};
-
-const init_board: Board = init: {
-    var b: Board = .{ .color = 0, .occupied = 0 };
-    b.set(3, 3, .white);
-    b.set(4, 4, .white);
-    b.set(3, 4, .black);
-    b.set(4, 3, .black);
-    break :init b;
-};
-
-const V2i = [2]i8; //@Vector(2, i8); ..selfhosted
-fn add(a: V2i, b: V2i) V2i {
-    return .{ a[0] + b[0], a[1] + b[1] };
-}
-fn inBounds(p: V2i, min: V2i, max: V2i) bool {
-    // @reduce(.And, @min(@max(p, V2i{ 0, 0 }), V2i{ 7, 7 }) == p))
-    return p[0] >= min[0] and p[0] <= max[0] and p[1] >= min[1] and p[1] <= max[1];
-}
-
-const compass_dirs: []const V2i = &.{
-    .{ 1, 0 }, .{ -1, 0 }, .{ 0, 1 },  .{ 0, -1 },
-    .{ 1, 1 }, .{ -1, 1 }, .{ 1, -1 }, .{ -1, -1 },
-};
-
-fn computeValidSquares(b: Board, col: Color) u64 {
-    assert(col != .empty);
-    var valid: u64 = 0;
-    for (0..8) |y| {
-        for (0..8) |x| {
-            if (b.get(x, y) != .empty) continue;
-
-            const ok: bool = loop: for (compass_dirs) |d| {
-                var p: V2i = .{ @intCast(x), @intCast(y) };
-                var has_oppo = false;
-                while (true) {
-                    p = add(p, d);
-                    if (!inBounds(p, V2i{ 0, 0 }, V2i{ 7, 7 })) continue :loop;
-                    const sq = b.get(p[0], p[1]);
-                    if (sq == .empty) continue :loop;
-                    if (sq == col and !has_oppo) continue :loop;
-                    if (sq == col and has_oppo) break :loop true;
-                    if (sq != col) has_oppo = true;
-                }
-            } else false;
-
-            if (ok) {
-                valid |= Board.bitmask(x, y);
-            }
-        }
-    }
-    return valid;
-}
-
-fn play(b: Board, p: V2i, col: Color) !Board {
-    assert(col != .empty);
-    const valid = computeValidSquares(b, col);
-    const bit = Board.bitmask(p[0], p[1]);
-    if (valid & bit == 0) return error.invalid;
-    var b1 = b;
-    b1.set(p[0], p[1], col);
-
-    loop: for (compass_dirs) |d| {
-        var p1 = p;
-        while (true) {
-            p1 = add(p1, d);
-            if (!inBounds(p1, V2i{ 0, 0 }, V2i{ 7, 7 })) continue :loop;
-            const sq = b.get(p1[0], p1[1]);
-            if (sq == .empty) continue :loop;
-            if (sq == col) break;
-        }
-
-        p1 = p;
-        while (true) {
-            p1 = add(p1, d);
-            assert(inBounds(p1, V2i{ 0, 0 }, V2i{ 7, 7 }));
-            const sq = b.get(p1[0], p1[1]);
-            assert(sq != .empty);
-            if (sq == col) continue :loop;
-            b1.set(p1[0], p1[1], col);
-        }
-    }
-    return b1;
 }
---
+++ src/othello.zig
@@ -0,0 +1,128 @@
+const std = @import("std");
+const assert = std.debug.assert;
+
+pub const Coord = [2]i8; //@Vector(2, i8); ..selfhosted
+fn add(a: Coord, b: Coord) Coord {
+    return .{ a[0] + b[0], a[1] + b[1] };
+}
+fn inBounds(p: Coord, min: Coord, max: Coord) bool {
+    // @reduce(.And, @min(@max(p, V2i{ 0, 0 }), V2i{ 7, 7 }) == p))
+    return p[0] >= min[0] and p[0] <= max[0] and p[1] >= min[1] and p[1] <= max[1];
+}
+
+pub fn bitmask(x: anytype, y: anytype) u64 {
+    assert(x >= 0 and y >= 0 and x < 8 and y < 8);
+    return @as(u64, 1) << @intCast(y * 8 + x);
+}
+
+pub const Color = enum { empty, black, white };
+pub const Board = struct {
+    occupied: u64,
+    color: u64,
+
+    pub fn get(b: @This(), x: anytype, y: anytype) Color {
+        const bit: u64 = bitmask(x, y);
+        if ((b.occupied & bit) == 0) return .empty;
+        return if ((b.color & bit) == 0) .black else .white;
+    }
+    pub fn set(b: *@This(), x: anytype, y: anytype, c: Color) void {
+        const bit: u64 = bitmask(x, y);
+        switch (c) {
+            .empty => b.occupied &= ~bit,
+            .black => {
+                b.occupied |= bit;
+                b.color &= ~bit;
+            },
+            .white => {
+                b.occupied |= bit;
+                b.color |= bit;
+            },
+        }
+    }
+};
+
+pub const init_board: Board = init: {
+    var b: Board = .{ .color = 0, .occupied = 0 };
+    b.set(3, 3, .white);
+    b.set(4, 4, .white);
+    b.set(3, 4, .black);
+    b.set(4, 3, .black);
+    break :init b;
+};
+
+const compass_dirs: []const Coord = &.{
+    .{ 1, 0 }, .{ -1, 0 }, .{ 0, 1 },  .{ 0, -1 },
+    .{ 1, 1 }, .{ -1, 1 }, .{ 1, -1 }, .{ -1, -1 },
+};
+
+pub fn computeValidSquares(b: Board, col: Color) u64 {
+    assert(col != .empty);
+    var valid: u64 = 0;
+    for (0..8) |y| {
+        for (0..8) |x| {
+            if (b.get(x, y) != .empty) continue;
+
+            const ok: bool = loop: for (compass_dirs) |d| {
+                var p: Coord = .{ @intCast(x), @intCast(y) };
+                var has_oppo = false;
+                while (true) {
+                    p = add(p, d);
+                    if (!inBounds(p, Coord{ 0, 0 }, Coord{ 7, 7 })) continue :loop;
+                    const sq = b.get(p[0], p[1]);
+                    if (sq == .empty) continue :loop;
+                    if (sq == col and !has_oppo) continue :loop;
+                    if (sq == col and has_oppo) break :loop true;
+                    if (sq != col) has_oppo = true;
+                }
+            } else false;
+
+            if (ok) {
+                valid |= bitmask(x, y);
+            }
+        }
+    }
+    return valid;
+}
+
+pub fn playAt(b: Board, p: Coord, col: Color) !Board {
+    assert(col != .empty);
+    const valid = computeValidSquares(b, col);
+    const bit = bitmask(p[0], p[1]);
+    if (valid & bit == 0) return error.invalid;
+    var b1 = b;
+    b1.set(p[0], p[1], col);
+
+    loop: for (compass_dirs) |d| {
+        var p1 = p;
+        while (true) {
+            p1 = add(p1, d);
+            if (!inBounds(p1, Coord{ 0, 0 }, Coord{ 7, 7 })) continue :loop;
+            const sq = b.get(p1[0], p1[1]);
+            if (sq == .empty) continue :loop;
+            if (sq == col) break;
+        }
+
+        p1 = p;
+        while (true) {
+            p1 = add(p1, d);
+            assert(inBounds(p1, Coord{ 0, 0 }, Coord{ 7, 7 }));
+            const sq = b.get(p1[0], p1[1]);
+            assert(sq != .empty);
+            if (sq == col) continue :loop;
+            b1.set(p1[0], p1[1], col);
+        }
+    }
+    return b1;
+}
+
+pub fn computeScore(b: Board) struct { whites: u32, blacks: u32 } {
+    var nbw: u32 = 0;
+    var nbb: u32 = 0;
+    var bit: u64 = 1;
+    for (0..64) |_| {
+        nbb += @intFromBool((b.occupied & bit != 0) and (b.color & bit == 0));
+        nbw += @intFromBool((b.occupied & bit != 0) and (b.color & bit != 0));
+        bit <<= 1;
+    }
+    return .{ .whites = nbw, .blacks = nbb };
+}
