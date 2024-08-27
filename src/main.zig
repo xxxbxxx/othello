a267212b608f38e59dfc6dc02f48a905ee134866
Xavier Bouchoux <xavierb@gmail.com>
2024-08-27 09:24:28 +0200

    affiche les pions

--- src/main.zig
+++ src/main.zig
@@ -1,9 +1,47 @@
 // raylib-zig (c) Nikolas Wipper 2023
-
+const std = @import("std");
 const rl = @import("raylib");
 const gui = @import("raygui");
 
+const assert = std.debug.assert;
+
+const Color = enum { empty, black, white };
 const Vec2 = rl.Vector2;
+const Board = struct {
+    occupied: u64,
+    color: u64,
+
+    fn get(b: @This(), x: usize, y: usize) Color {
+        assert(x < 8 and y < 8);
+        const bit: u64 = @as(u64, 1) << @intCast(y * 8 + x);
+        if ((b.occupied & bit) == 0) return .empty;
+        return if ((b.color & bit) == 0) .black else .white;
+    }
+    fn set(b: *@This(), x: usize, y: usize, c: Color) void {
+        assert(x < 8 and y < 8);
+        const bit: u64 = @as(u64, 1) << @intCast(y * 8 + x);
+        switch (c) {
+            .empty => b.occupied &= ~bit,
+            .black => {
+                b.occupied |= bit;
+                b.color &= ~bit;
+            },
+            .white => {
+                b.occupied |= bit;
+                b.color |= bit;
+            },
+        }
+    }
+};
+
+const init_board: Board = init: {
+    var b: Board = .{ .color = 0, .occupied = 0 };
+    b.set(3, 3, .white);
+    b.set(4, 4, .white);
+    b.set(3, 4, .black);
+    b.set(4, 3, .black);
+    break :init b;
+};
 
 pub fn main() anyerror!void {
     // Initialization
@@ -31,7 +69,7 @@ pub fn main() anyerror!void {
         defer rl.endDrawing();
 
         rl.clearBackground(rl.Color.dark_brown);
-        drawBoard(.{ .x = 20, .y = 20 }, 640);
+        drawBoard(init_board, .{ .x = 20, .y = 20 }, 640);
 
         //            rl.drawCircle(200, 200, 100, rl.Color.red);
         rl.drawText("Congrats! You created your first window!", 190, 200, 20, rl.Color.light_gray);
@@ -46,30 +84,47 @@ pub fn main() anyerror!void {
 fn addmul(p: Vec2, k: f32, d: Vec2) Vec2 {
     return Vec2.add(p, Vec2.multiply(d, .{ .x = k, .y = k }));
 }
-fn drawBoard(pos: Vec2, size: f32) void {
+fn drawBoard(b: Board, pos: Vec2, size: f32) void {
     const rect: rl.Rectangle = .{ .x = pos.x, .y = pos.y, .width = size, .height = size };
     rl.drawRectangleRounded(rect, 0.1, 7, rl.Color.dark_green);
 
-    const pos0 = pos;
-    const posw = Vec2.add(pos, .{ .x = size, .y = 0 });
-    const posh = Vec2.add(pos, .{ .x = 0, .y = size });
-    for (0..8) |i| {
-        if (i == 0) continue;
-        rl.drawLineEx(
-            addmul(pos0, size / 8, .{ .x = @floatFromInt(i), .y = 0 }),
-            addmul(posh, size / 8, .{ .x = @floatFromInt(i), .y = 0 }),
-            2.0,
-            rl.Color.black,
-        );
-        rl.drawLineEx(
-            addmul(pos0, size / 8, .{ .y = @floatFromInt(i), .x = 0 }),
-            addmul(posw, size / 8, .{ .y = @floatFromInt(i), .x = 0 }),
-            2.0,
-            rl.Color.black,
-        );
+    {
+        const pos0 = pos;
+        const posw = Vec2.add(pos, .{ .x = size, .y = 0 });
+        const posh = Vec2.add(pos, .{ .x = 0, .y = size });
+        for (0..8) |i| {
+            if (i == 0) continue;
+            rl.drawLineEx(
+                addmul(pos0, size / 8, .{ .x = @floatFromInt(i), .y = 0 }),
+                addmul(posh, size / 8, .{ .x = @floatFromInt(i), .y = 0 }),
+                2.0,
+                rl.Color.black,
+            );
+            rl.drawLineEx(
+                addmul(pos0, size / 8, .{ .y = @floatFromInt(i), .x = 0 }),
+                addmul(posw, size / 8, .{ .y = @floatFromInt(i), .x = 0 }),
+                2.0,
+                rl.Color.black,
+            );
+        }
+    }
+
+    {
+        const pos0 = Vec2.add(pos, .{ .x = size / 16, .y = size / 16 });
+
+        for (0..8) |y| {
+            for (0..8) |x| {
+                const col = switch (b.get(x, y)) {
+                    .empty => continue,
+                    .white => rl.Color.beige,
+                    .black => rl.Color.dark_brown,
+                };
+
+                rl.drawCircleV(addmul(pos0, size / 8, .{ .x = @floatFromInt(x), .y = @floatFromInt(y) }), size / 20, col);
+                rl.drawCircleLinesV(addmul(pos0, size / 8, .{ .x = @floatFromInt(x), .y = @floatFromInt(y) }), size / 20, rl.Color.black);
+            }
+        }
     }
 
     rl.drawRectangleRoundedLinesEx(rect, 0.1, 7, 5.0, rl.Color.black);
-
-    //rl.drawCircle(200, 200, 100, rl.Color.red);
 }
